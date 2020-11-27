04c7a00b97673bc1888bef30445190663219b0a8
Leandro Costa <lti@pop-os.localdomain>
2020-11-27 09:08:11 -0300

    Feita a conexão com o BD MySql

---
+++ config/conexao.php
@@ -0,0 +1,15 @@
+<?php
+try{
+
+    DEFINE('HOST','localhost');
+    DEFINE('BD','new_agenda');
+    DEFINE('USER','root');
+    DEFINE('PASS','lti');
+
+    $conect = new PDO('mysql:host='.HOST.';dbname='.BD,USER,PASS);
+    $conect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    echo "<strong>ERRO DE PDO = </strong>".$e->getMessage();
+}
+    
